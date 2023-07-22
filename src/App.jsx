68a2ebd82bf3f79bcc68ea4e58622a185a9d5ce0
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-22 15:10:25 +0200

    añadidos filtros

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import Products from "./components/Products";
 import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 import { products as initialProducts } from "./mocks/products.json";
 
 function useFilters() {
@@ -29,6 +30,7 @@ function App() {
 
   return (
     <>
+      <Header changeFilters={setFilters} />
       <Products products={filteredProducts} />
       <Footer />
     </>
