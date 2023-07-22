ad8c456f79901ed693196b44d609ebe0000d6956
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-22 14:50:41 +0200

    mapeo mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import Products from "./components/Products";
+import { products as initialProducts } from "./mocks/products.json";
 
 function App() {
+  const [products] = useState(initialProducts);
+  
   return (
     <>
-      <Products />
+      <Products products={products} />
     </>
   );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,26 @@
+import { AddToCartIcon } from "./Icons.jsx";
+import "./Products.css";
 
-
-function Products() {
+function Products({ products }) {
   return (
-    <div>Products</div>
-  )
+    <main className="products">
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
+            <div>
+              <strong>{product.title}</strong> - ${product.price}
+            </div>
+            <div>
+              <button>
+                <AddToCartIcon />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
-export default Products
+export default Products;
