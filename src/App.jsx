674f4249d6f5005310e574f817804e228d0b5ed1
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-22 14:55:14 +0200

    añadimos filtro

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
 import Products from "./components/Products";
 import { products as initialProducts } from "./mocks/products.json";
 
 function App() {
   const [products] = useState(initialProducts);
-  
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
+
+  const filteredProducts = filterProducts(products);
+
   return (
     <>
-      <Products products={products} />
+      <Products products={filteredProducts} />
     </>
   );
 }
