96ce1b934493db973f287d5d9874e619e79c0851
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-23 22:42:48 +0200

    modificado productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-import Products from "./components/Products";
+import { Products } from "./components/Products";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { products as initialProducts } from "./mocks/products.json";
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,26 +1,42 @@
-import { AddToCartIcon } from "./Icons.jsx";
 import "./Products.css";
+import { AddToCartIcon, RemoveFromCartIcon } from "./Icons.jsx";
+import { useCart } from "../hooks/useCart.js";
 
-function Products({ products }) {
+export function Products({ products }) {
+  const { addToCart, removeFromCart, cart } = useCart();
+
+  const checkProductInCart = (product) => {
+    return cart.some((item) => item.id === product.id);
+  };
+
   return (
     <main className="products">
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <img src={product.thumbnail} alt={product.title} />
-            <div>
-              <strong>{product.title}</strong> - ${product.price}
-            </div>
-            <div>
-              <button>
-                <AddToCartIcon />
-              </button>
-            </div>
-          </li>
-        ))}
+        {products.slice(0, 10).map((product) => {
+          const isProductInCart = checkProductInCart(product);
+
+          return (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <div>
+                <strong>{product.title}</strong> - ${product.price}
+              </div>
+              <div>
+                <button
+                  style={{ backgroundColor: isProductInCart ? "red" : "cyan" }}
+                  onClick={() => {
+                    isProductInCart
+                      ? removeFromCart(product)
+                      : addToCart(product);
+                  }}
+                >
+                  {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
+                </button>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
 }
-
-export default Products;
